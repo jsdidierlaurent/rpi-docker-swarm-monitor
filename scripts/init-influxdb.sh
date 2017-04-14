bf252dfad5747f8d1d29daffee0d008f8beadc7c
Jean-Sébastien Didierlaurent <js.didierlaurent@gmail.com>
2017-04-14 20:51:08 +0200

    Adding retention policy on init-influxdb.sh

--- scripts/init-influxdb.sh
+++ scripts/init-influxdb.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker exec `docker ps | grep -i influx | awk '{print $1}'` influx -execute 'CREATE DATABASE cadvisor'
+docker exec `docker ps | grep -i influx | awk '{print $1}'` influx -execute 'CREATE RETENTION POLICY one_day_only ON cadvisor DURATION 1d REPLICATION 1 DEFAULT'
